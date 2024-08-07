c3afd8168fc7b6c274168c2256f29016c367b558
JackS247 <jacksnape89@gmail.com>
2024-08-07 20:02:30 +0100

    Update page.js

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -2,7 +2,74 @@ export default function PrivacyPolicy() {
   return (
     <div className="text-center pt-28">
       <h1>Privacy Policy</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam</p>
+      <p>Last updated August 07, 2024</p>
+
+      <p>
+        This privacy notice for Friend Ferry ltd ('we', 'us', or 'our'),
+        describes how and why we might collect, store, use, and/or share
+        ('process') your information when you use our services ('Services'),
+        such as when you:
+      </p>
+
+      <ul>
+        <li>
+          Visit our website at{" "}
+          <a href="https://friendferry.com/">https://friendferry.com/</a>, or
+          any website of ours that links to this privacy notice
+        </li>
+        <li>
+          Engage with us in other related ways, including any sales, marketing,
+          or events
+        </li>
+      </ul>
+
+      <p>
+        Questions or concerns? Reading this privacy notice will help you
+        understand your privacy rights and choices. If you do not agree with our
+        policies and practices, please do not use our Services. If you still
+        have any questions or concerns, please contact us at{" "}
+        <a href="mailto:admin@friendferry.com">admin@friendferry.com</a>.
+      </p>
+
+      <h2>SUMMARY OF KEY POINTS</h2>
+
+      <p>
+        This summary provides key points from our privacy notice, but you can
+        find out more details about any of these topics by clicking the link
+        following each key point or by using our table of contents below to find
+        the section you are looking for.
+      </p>
+
+      <ul>
+        <li>
+          <strong>What personal information do we process?</strong> When you
+          visit, use, or navigate our Services, we may process personal
+          information depending on how you interact with us and the Services,
+          the choices you make, and the products and features you use.{" "}
+          <a href="#personal-info">
+            Learn more about personal information you disclose to us.
+          </a>
+        </li>
+        <li>
+          <strong>Do we process any sensitive personal information?</strong> We
+          do not process sensitive personal information.
+        </li>
+        <li>
+          <strong>Do we collect any information from third parties?</strong> We
+          do not collect any information from third parties.
+        </li>
+        <li>
+          <strong>How do we process your information?</strong> We process your
+          information to provide, improve, and administer our Services,
+          communicate with you, for security and fraud prevention, and to comply
+          with law. We may also process your information for other purposes with
+          your consent. We process your information only when we have a valid
+          legal reason to do so.{" "}
+          <a href="#how-we-process">
+            Learn more about how we process your information.
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
