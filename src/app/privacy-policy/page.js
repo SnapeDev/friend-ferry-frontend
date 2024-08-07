a641e64a9cb7c0d82a5725a62b17a24f42be35d4
JackS247 <jacksnape89@gmail.com>
2024-08-07 19:17:23 +0100

    privacy

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -1,8 +1,26 @@
+import { useEffect } from "react";
+
 export default function PrivacyPolicy() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.id = "termly-jssdk";
+    script.src = "https://app.termly.io/embed-policy.min.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="text-center pt-28">
       <h1>Privacy Policy</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam</p>
+      <div
+        name="termly-embed"
+        data-id="2af0c5c8-cdab-40bf-8ed7-7b53eb40c695"
+      ></div>
     </div>
   );
 }
