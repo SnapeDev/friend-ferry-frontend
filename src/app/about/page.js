0e63a1026ab8e4976f22cce09ddcf82c23b38b46
JackS247 <jacksnape89@gmail.com>
2024-08-07 12:12:20 +0100

    Revert "Update page.js"
    This reverts commit 0ebebb410efd3aa8f591945c293045fb4a4da475.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,28 +1,8 @@
 export default function About() {
-  return (
-    <div className="text-center pt-28">
-      <h1 className="text-5xl font-serif">About</h1>
-      <p className="text-lg font-sans mt-4">
-        Friend Ferry is a unique online service dedicated to connecting women
-        with trustworthy and respectful men in their area for enjoyable and
-        meaningful social interactions. Whether you need a friend to accompany
-        you to an event, a companion for a coffee date, or someone to accompany
-        you on any outing, Friend Ferry ensures you have the perfect company for
-        the occasion. Our service is designed with safety, convenience, and
-        enjoyment in mind. All our male companions are thoroughly vetted to
-        guarantee a safe and pleasant experience. We understand the importance
-        of having a reliable friend by your side, and our mission is to provide
-        just that. Friend Ferry is great for various reasons. It allows women to
-        explore new social opportunities without the pressure of romantic
-        expectations. Whether you're new to a city, looking to expand your
-        social circle, or simply want someone to share an experience with,
-        Friend Ferry offers a reliable solution. With a user-friendly platform,
-        you can easily browse profiles and select the perfect companion for your
-        needs. Our service is flexible, accommodating both spontaneous plans and
-        well-planned events. We pride ourselves on fostering genuine connections
-        and memorable experiences. Join the Friend Ferry community today, your
-        perfect outing is just a click away!
-      </p>
-    </div>
-  );
+	return (
+		<div className="text-center pt-28">
+			<h1>About</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam</p>
+		</div>
+	);
 }
