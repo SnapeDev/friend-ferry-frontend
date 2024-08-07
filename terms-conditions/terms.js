fc6eeef5c8b730c906942dd610360038aa44d752
JackS247 <jacksnape89@gmail.com>
2024-08-07 14:49:13 +0100

    terms

---
+++ terms-conditions/terms.js
@@ -0,0 +1,8 @@
+export default function TermsConditions() {
+  return (
+    <div className="text-center pt-28">
+      <h1>Terms & Conditions</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam</p>
+    </div>
+  );
+}
