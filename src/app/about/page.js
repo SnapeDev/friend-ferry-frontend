c0365283996f0f9d5b21fc73761aa15c4b1b95cf
JackS247 <jacksnape89@gmail.com>
2024-08-07 14:01:57 +0100

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,15 +2,15 @@ export default function About() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="max-w-3xl text-center p-8">
-        <h1 className="text-4xl font-medium mb-6">About</h1>
+        <h1 className="text4xl text-primary font-bold">friend ferry</h1>
         <p className="text-lg font-medium leading-relaxed">
           Friend Ferry is a unique online service dedicated to connecting women
           with trustworthy and respectful men in their area for enjoyable and
           meaningful social interactions. Whether you need a friend to accompany
           you to an event, a companion for a coffee date, or someone to
           accompany you on any outing, Friend Ferry ensures you have the perfect
-          company for the occasion. Our service is designed with safety,
-          convenience, and enjoyment in mind. All our male companions are
+          company for the occasion.<br></br> Our service is designed with
+          safety, convenience, and enjoyment in mind. All our guys are
           thoroughly vetted to guarantee a safe and pleasant experience. We
           understand the importance of having a reliable friend by your side,
           and our mission is to provide just that. Friend Ferry is great for
@@ -22,8 +22,9 @@ export default function About() {
           profiles and select the perfect companion for your needs. Our service
           is flexible, accommodating both spontaneous plans and well-planned
           events. We pride ourselves on fostering genuine connections and
-          memorable experiences. Join the Friend Ferry community today, your
-          perfect outing is just a click away!
+          memorable experiences.<br></br>
+          Join the Friend Ferry community today, your perfect outing is just a
+          click away!
         </p>
       </div>
     </div>
