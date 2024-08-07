b1839e22d6f5ff789aa8147e6856f45c9c996a2f
JackS247 <jacksnape89@gmail.com>
2024-08-07 14:43:03 +0100

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,25 +5,25 @@ export default function About() {
         <h1 className="text-3xl text-primary font-bold mb-8">About Us</h1>
         <p className="text-lg font-medium leading-relaxed">
           Friend Ferry is a unique online service dedicated to connecting women
-          with trustworthy and respectful men in their area for enjoyable and
-          meaningful social interactions. Whether you need a friend to accompany
-          you to an event, a companion for a coffee date, or someone to
-          accompany you on any outing, Friend Ferry ensures you have the perfect
-          company for the occasion. Our service is designed with safety,
-          convenience, and enjoyment in mind. All our guys are thoroughly vetted
-          to guarantee a safe and pleasant experience. We understand the
-          importance of having a reliable friend by your side, and our mission
-          is to provide just that. Friend Ferry is great for various reasons. It
-          allows women to explore new social opportunities without the pressure
-          of romantic expectations. Whether you&apos;re new to a city, looking
-          to expand your social circle, or simply want someone to share an
-          experience with, Friend Ferry offers a reliable solution. With a
-          user-friendly platform, you can easily browse profiles and select the
-          perfect companion for your needs. Our service is flexible,
-          accommodating both spontaneous plans and well-planned events. We pride
-          ourselves on fostering genuine connections and memorable experiences.
-          Join the Friend Ferry community today, your perfect outing is just a
-          click away!
+          with men in their area for enjoyable and meaningful social
+          interactions. Whether you need a friend to accompany you to an event,
+          a companion for a coffee date, or someone to accompany you on any
+          outing, Friend Ferry ensures you have the perfect company for the
+          occasion. Our service is designed with safety, convenience, and
+          enjoyment in mind. All our guys are thoroughly vetted to guarantee a
+          safe and pleasant experience. We understand the importance of having a
+          reliable friend by your side, and our mission is to provide just that.
+          Friend Ferry is great for various reasons. It allows women to explore
+          new social opportunities without the pressure of romantic
+          expectations. Whether you&apos;re new to a city, looking to expand
+          your social circle, or simply want someone to share an experience
+          with, Friend Ferry offers a reliable solution. With a user-friendly
+          platform, you can easily browse profiles and select the perfect
+          companion for your needs. Our service is flexible, accommodating both
+          spontaneous plans and well-planned events. We pride ourselves on
+          fostering genuine connections and memorable experiences. Join the
+          Friend Ferry community today, your perfect outing is just a click
+          away!
         </p>
       </div>
     </div>
