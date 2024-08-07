e1247d0c6251bc2284fbe30e615f50e27b772d40
JackS247 <jacksnape89@gmail.com>
2024-08-07 12:59:17 +0100

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,8 @@
 export default function About() {
-	return (
-		<div className="text-center pt-28">
-			<h1>About</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam</p>
-		</div>
-	);
+  return (
+    <div className="text-center pt-28">
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Null</p>
+    </div>
+  );
 }
