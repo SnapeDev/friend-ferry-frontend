35b4d322f9ecc3d26daf1bbe541aacea388e757a
JackS247 <jacksnape89@gmail.com>
2024-08-07 19:00:44 +0100

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,13 +1,12 @@
 export default function Contact() {
   return (
-    <div className="text-center pt-28">
-      <p className="text-medium">
+    <div className="text-center pt-64">
+      <p className="text-xl">
         <a href="mailto:admin@friendferry.com" className="text-primary">
           admin@friendferry.com
         </a>{" "}
         <br></br>
         <br></br>
-        Friend Ferry Ltd<br></br>
         71-75 Shelton Street<br></br>
         Covent Garden<br></br>
         WC2H 9JQ<br></br>
