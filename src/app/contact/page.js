34d509bd2bbd82193d333802f87ed0b323d3b309
JackS247 <jacksnape89@gmail.com>
2024-08-07 18:51:56 +0100

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,15 @@ export default function PrivacyPolicy() {
   return (
     <div className="text-center pt-28">
       <h1>Privacy Policy</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam</p>
+      <p>
+        admin@friendferry.com <br></br>
+        <br></br>
+        71-75 Shelton Street<br></br>
+        Covent Garden<br></br>
+        WC2H 9JQ<br></br>
+        London<br></br>
+        United Kingdom
+      </p>
     </div>
   );
 }
