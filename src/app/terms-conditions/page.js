86e880d49ff7c38162b13da3ceacf72a5edcc706
JackS247 <jacksnape89@gmail.com>
2024-08-08 11:22:50 +0100

    Update page.js

--- src/app/terms-conditions/page.js
+++ src/app/terms-conditions/page.js
@@ -7,74 +7,71 @@ export default function TermsAndConditions() {
       </h1>
 
       <p className="mb-4">
-        Please read the following terms and conditions of use carefully before
-        using this website. By accessing and using this site, you agree to be
-        bound by the following terms and conditions and all applicable laws. If
-        you do not agree to these terms and conditions, please do not use this
-        site.
+        Please read these terms and conditions carefully before using this
+        website. By accessing and using this site, you agree to be bound by the
+        terms and conditions set forth herein and all applicable laws. If you do
+        not agree to these terms and conditions, please do not use this site.
       </p>
 
       <h2 className="text-2xl font-bold mt-6 mb-4">Prohibited Services</h2>
       <p className="mb-4">
-        Friend Ferry Ltd prioritises the safety of our clients. The companions
-        provided by Friend Ferry are professional and charming models. They are
-        strictly prohibited from performing any intimate or inappropriate acts
-        for guests, event hosts, or party attendees. Soliciting such services
-        from our companions is not allowed under any circumstances.
+        Friend Ferry Ltd prioritises the safety of our clients. Our companions
+        are professional and charming models who are strictly prohibited from
+        engaging in any intimate or inappropriate acts with guests, event hosts,
+        or party attendees. Soliciting such services from our companions is
+        strictly forbidden under any circumstances.
       </p>
 
-      <h2 className="text-2xl font-bold mt-6 mb-4">COPYRIGHT</h2>
+      <h2 className="text-2xl font-bold mt-6 mb-4">Copyright</h2>
       <p className="mb-4">
         All content on this site, including but not limited to text, graphics,
         and code, is protected by copyright laws as a collective work under
-        United Kingdom copyright laws, and is the property of Friend Ferry Ltd.
+        United Kingdom copyright laws and is the property of Friend Ferry Ltd.
         This collective work includes works licensed to Friend Ferry Ltd.
         Copyright 2023, Friend Ferry Ltd. ALL RIGHTS RESERVED. Permission is
         granted to electronically copy and print hard copy portions of this site
-        for the sole purpose of placing an order with Friend Ferry Ltd or
-        purchasing Friend Ferry Ltd products. You may display, download, or
-        print portions of the material from different areas of the site for your
-        own non-commercial use, or to place an order with Friend Ferry Ltd or
-        purchase Friend Ferry Ltd products, subject to any expressly stated
-        restrictions or limitations relating to specific material. Any other
-        use, including but not limited to the reproduction, distribution,
-        display, or transmission of the content of this site, is strictly
-        prohibited unless authorised by Friend Ferry Ltd. You also agree not to
-        alter or remove any proprietary notices from materials downloaded from
-        the site.
+        solely for placing an order with Friend Ferry Ltd or purchasing Friend
+        Ferry Ltd products. You may display, download, or print portions of the
+        material from different areas of the site for your own non-commercial
+        use, or to place an order with Friend Ferry Ltd or purchase Friend Ferry
+        Ltd products, subject to any expressly stated restrictions or
+        limitations relating to specific material. Any other use, including but
+        not limited to the reproduction, distribution, display, or transmission
+        of the content of this site, is strictly prohibited unless authorized by
+        Friend Ferry Ltd. You also agree not to alter or remove any proprietary
+        notices from materials downloaded from the site.
       </p>
 
-      <h2 className="text-2xl font-bold mt-6 mb-4">WARRANTY DISCLAIMER</h2>
+      <h2 className="text-2xl font-bold mt-6 mb-4">Warranty Disclaimer</h2>
       <p className="mb-4">
         This site and the materials and products on this site are provided
         &quot;as is&quot; and without warranties of any kind, whether express or
-        implied. To the fullest extent permissible pursuant to applicable law,
-        Friend Ferry Ltd disclaims all warranties, express or implied,
-        including, but not limited to, implied warranties of merchantability and
-        fitness for a particular purpose and non-infringement. Friend Ferry Ltd
-        does not warrant that the functions contained in the site will be
-        uninterrupted or error-free, that defects will be corrected, or that
-        this site or the server that makes the site available are free of
-        viruses or other harmful components. Friend Ferry Ltd makes no
-        warranties or representations regarding the use of the materials on this
-        site in terms of their correctness, accuracy, adequacy, usefulness,
-        timeliness, reliability, or otherwise. Some states do not permit
-        limitations or exclusions on warranties, so the above limitations may
-        not apply to you.
+        implied. To the fullest extent permissible under applicable law, Friend
+        Ferry Ltd disclaims all warranties, express or implied, including, but
+        not limited to, implied warranties of merchantability, fitness for a
+        particular purpose, and non-infringement. Friend Ferry Ltd does not
+        warrant that the functions contained in the site will be uninterrupted
+        or error-free, that defects will be corrected, or that this site or the
+        server that makes it available are free of viruses or other harmful
+        components. Friend Ferry Ltd makes no warranties or representations
+        regarding the use of the materials on this site in terms of their
+        correctness, accuracy, adequacy, usefulness, timeliness, reliability, or
+        otherwise. Some jurisdictions do not allow limitations or exclusions on
+        warranties, so the above limitations may not apply to you.
       </p>
 
-      <h2 className="text-2xl font-bold mt-6 mb-4">LIMITATION OF LIABILITY</h2>
+      <h2 className="text-2xl font-bold mt-6 mb-4">Limitation of Liability</h2>
       <p className="mb-4">
         Friend Ferry Ltd shall not be liable for any special or consequential
         damages that result from the use of, or the inability to use, the
         materials on this site or the performance of the products, even if
         Friend Ferry Ltd has been advised of the possibility of such damages.
-        Applicable law may not allow the limitation of exclusion of liability or
-        incidental or consequential damages, so the above limitation or
+        Applicable law may not allow the limitation or exclusion of liability
+        for incidental or consequential damages, so the above limitation or
         exclusion may not apply to you.
       </p>
 
-      <h2 className="text-2xl font-bold mt-6 mb-4">TYPOGRAPHICAL ERRORS</h2>
+      <h2 className="text-2xl font-bold mt-6 mb-4">Typographical Errors</h2>
       <p className="mb-4">
         In the event that a Friend Ferry Ltd product is mistakenly listed at an
         incorrect price, Friend Ferry Ltd reserves the right to refuse or cancel
@@ -86,7 +83,7 @@ export default function TermsAndConditions() {
         account in the amount of the incorrect price.
       </p>
 
-      <h2 className="text-2xl font-bold mt-6 mb-4">TERM, TERMINATION</h2>
+      <h2 className="text-2xl font-bold mt-6 mb-4">Termination</h2>
       <p className="mb-4">
         These terms and conditions are applicable to you upon accessing the site
         and/or completing the registration or shopping process. Friend Ferry Ltd
@@ -96,7 +93,7 @@ export default function TermsAndConditions() {
         Indemnification, and Miscellaneous shall survive any termination.
       </p>
 
-      <h2 className="text-2xl font-bold mt-6 mb-4">NOTICE</h2>
+      <h2 className="text-2xl font-bold mt-6 mb-4">Notice</h2>
       <p className="mb-4">
         Friend Ferry Ltd may deliver notice to you via e-mail, a general notice
         on the site, or by other reliable methods to the address you have
